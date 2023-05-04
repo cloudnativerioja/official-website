b07807cbaa1ff11bd2175df647bfeb358647e6e1
Diego Cristóbal Herreros <diego.cristobal@teamcmp.com>
2023-05-04 12:59:10 +0200

    Feat: set next.config.js to use imgix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: "imgix",
+    path: "cloudnativerioja.imgix.net",
   },
-  output: "standalone",
 };
