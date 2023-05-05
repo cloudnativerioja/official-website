decde7f512c1334fda3c418c860a0a4b9bb8a567
statickidz <statickidz@gmail.com>
2023-05-05 10:17:24 +0000

    Deploying to gh-pages from @ cloudnativerioja/official-website@ac76b1334039b988ede3d0d0752ae9a0cfa0c26d 🚀

---
+++ _next/static/jrAINPVNAV9xwiA6b2U-g/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/432-26a34156802e8f1e.js","static/chunks/pages/index-ebf67cd4e15989d3.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/twVZiaHqI2RA8wDrk2Mp0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/432-70f023998f9c8981.js","static/chunks/pages/index-f89c21f02d0cc84f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
