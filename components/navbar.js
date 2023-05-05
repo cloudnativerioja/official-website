c7def77da6f6974e184ab62d743f2debe8291f9b
Adrian Barrio <statickidz@gmail.com>
2023-05-05 11:37:59 +0200

    chore: rename logo svg

--- components/navbar.js
+++ components/navbar.js
@@ -19,7 +19,7 @@ const Navbar = () => {
                   <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
                     <span>
                       <Image
-                        src="img/logo.svg"
+                        src="img/logo-cncf-rioja.svg"
                         alt="N"
                         width="130"
                         height="50"
