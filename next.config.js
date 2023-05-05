867316132dc9a31c881497fc548f8b002abbdc55
Adrian Barrio <statickidz@gmail.com>
2023-05-05 08:28:48 +0200

    fix: remove assetPrefix and basePath for ghpages domain

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: "imgix",
-    path: "cloudnativerioja.imgix.net",
-  },
+  assetPrefix: '',
+  basePath: '',
 };
