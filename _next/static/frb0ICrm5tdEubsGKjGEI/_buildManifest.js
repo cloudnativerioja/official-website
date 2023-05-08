0a8b5fc3ceface6e2bd4c566dbf8c310d5d8f46a
dcristobalhMad <diego.cristobal@mad.io>
2023-05-08 14:53:56 +0000

    Deploying to gh-pages from @ cloudnativerioja/official-website@1893644316e49c881270c66c54519f674f4cd4e2 🚀

--- _next/static/frb0ICrm5tdEubsGKjGEI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/446-a7facdb5b17be29b.js","static/chunks/pages/index-38a7f801e9894fae.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
