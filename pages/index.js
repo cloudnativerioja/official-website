6132e9cf73d50425ac286ffe3379e16913e0da3e
Adrian Barrio <statickidz@gmail.com>
2023-06-04 19:13:50 +0200

    chore: remove unused code

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Hero from '../components/hero';
 import Navbar from '../components/navbar';
 import SectionTitle from '../components/sectionTitle';
 
-import { benefitOne, benefitTwo } from '../components/data';
+import { benefitOne } from '../components/data';
 import Video from '../components/video';
 import Benefits from '../components/benefits';
 import Footer from '../components/footer';
