a8768527665b0fda168d825f6b1c4a74aa8ea97f
statickidz <statickidz@gmail.com>
2023-07-02 16:30:07 +0000

    Deploying to gh-pages from @ cloudnativerioja/official-website@42ffa0be9e41c5f5faa0e3291bd192da51725c3c 🚀

--- _next/static/-e3agqwL-vj_2kuP-b4rD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/446-a7facdb5b17be29b.js","static/chunks/pages/index-a6ee8bfb2f33625e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
