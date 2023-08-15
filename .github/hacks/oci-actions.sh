12d0ed9ab71a80c3b460b3b36ba281bd87d81fc7
Adrian Barrio <statickidz@gmail.com>
2023-08-15 20:12:11 +0200

    fix(charts): push path

--- .github/hacks/oci-actions.sh
+++ .github/hacks/oci-actions.sh
@@ -30,7 +30,7 @@ function package() {
 # function push a chart to an OCI registry
 function push() {
     echo "Pushing chart to OCI registry..."
-    helm push ./${CHART_NAME}/${CHART_NAME}/${CHART_NAME}-${TAG}.tgz oci://${OCI_REGISTRY}/${OCI_NAMESPACE}
+    helm push ./${CHART_NAME}/${CHART_NAME}-${TAG}.tgz oci://${OCI_REGISTRY}/${OCI_NAMESPACE}
 }
 
 function main() {
