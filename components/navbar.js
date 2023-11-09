3d4742c32df2b2b09713e79f97ff1ab6c91c56ce
Adrian Barrio <statickidz@gmail.com>
2023-11-09 18:34:34 +0100

    fix(lint): errors

--- components/navbar.js
+++ components/navbar.js
@@ -32,27 +32,27 @@ const Navbar = () => {
   return (
     <div className="w-full">
       {/* annoucement  */}
-      <div class="md:mt-8 md:mb-8 flex flex-wrap items-center justify-center">
+      <div className="md:mt-8 md:mb-8 flex flex-wrap items-center justify-center">
         <Link
           href="https://community.cncf.io/events/details/cncf-cloud-native-rioja-presents-kubernetes-rioja-day/"
           target="_blank"
         >
           <div
-            class="p-2 bg-gray-700 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+            className="p-2 bg-gray-700 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
             role="alert"
           >
-            <span class="flex items-center justify-center rounded-full bg-cncf uppercase px-3 py-2 text-xs font-bold mr-3">
-              <span class="relative flex h-2 w-2 mr-2">
-                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-accent opacity-75"></span>
-                <span class="relative inline-flex rounded-full h-2 w-2 bg-accent"></span>
+            <span className="flex items-center justify-center rounded-full bg-cncf uppercase px-3 py-2 text-xs font-bold mr-3">
+              <span className="relative flex h-2 w-2 mr-2">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-accent opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-2 w-2 bg-accent"></span>
               </span>
               KUBERNETES Rioja DAY 2023
             </span>
-            <span class="font-semibold mr-2 text-left flex-auto">
+            <span className="font-semibold mr-2 text-left flex-auto">
               Únete al primer evento Cloud Native en La Rioja
             </span>
             <svg
-              class="fill-current opacity-75 h-4 w-4"
+              className="fill-current opacity-75 h-4 w-4"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
             >
